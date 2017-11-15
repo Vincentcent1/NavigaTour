1a86f44dd9c4401fe1297019f3276a15e11c7022
Koh Jing Yu <kohjingyu95@gmail.com>
2017-11-15 16:15:52 +0800

    Changed attractions.json to all strings and added to assets folder

--- app/src/main/java/com/example/android/navigatour/TSPActivity.java
+++ app/src/main/java/com/example/android/navigatour/TSPActivity.java
@@ -3,7 +3,10 @@ package com.example.android.navigatour;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//import Gson;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.HashMap;
 
 public class TSPActivity extends AppCompatActivity {
 
@@ -11,5 +14,8 @@ public class TSPActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tsp);
+
+        String jsonString = "Your JSON string";
+        HashMap<String,String> map = new Gson().fromJson(jsonString, new TypeToken<HashMap<String, String>>(){}.getType());
     }
 }
