6c4e2355c817b704ea01b542b0c56b381715740c
Vincentcent1 <Vincent@mymail.sutd.edu.sg>
2017-11-20 16:20:39 +0800

    Added Base ViaMichelin XML Parser

--- app/src/main/java/com/example/android/navigatour/LocateNearbyActivity.java
+++ app/src/main/java/com/example/android/navigatour/LocateNearbyActivity.java
@@ -3,6 +3,13 @@ package com.example.android.navigatour;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 /**
  * Created by setia on 11/15/2017.
  */
@@ -14,5 +21,48 @@ public class LocateNearbyActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locate_nearby);
     }
+
+    public static String performGetCall(String requestURL) {
+        URL url;
+        String response = "";
+        try {
+
+            url = new URL(requestURL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            int responseCode=conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line=br.readLine()) != null) {
+                    response += line;
+                }
+            }
+            else {
+                response = null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+
+//    public class GetRestaurantsTask extends AsyncTask<URL, Void, String> {
+//
+//        @Override
+//        protected String doInBackground(URL... urls) {
+//            URL requestURL = urls[0];
+//
+//        }
+//    }
+
+
 
 }
